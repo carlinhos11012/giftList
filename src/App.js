e251d7d1108f8448e0e3da7fb074b82161f5662d
carlinhos11012 <carlinhohm110@gmail.com>
2024-05-17 22:38:15 -0300

    teste soma

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="text-cyan-500">Save</div>;
+  const [number, setNumber] = useState(0);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-black text-4xl mb-4">{number}</div>
+      <div
+        className="bg-red-500 w-fit px-5 text-2xl rounded-2xl hover:cursor-pointer select-none"
+        onClick={() => {
+          setNumber(number + 1);
+        }}
+      >
+        Botão
+      </div>
+    </div>
+  );
 }
 
 export default App;
